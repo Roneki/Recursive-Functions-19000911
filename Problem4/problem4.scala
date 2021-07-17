900386f7b7e5a8b46a032aca08cfd09ae03a0e48
Roneki Saranga Manamperi <roneki.saranga12@gmail.com>
2021-07-18 04:47:10 +0530

    Problem4

---
+++ Problem4/problem4.scala
@@ -0,0 +1,10 @@
+object problem4 extends App{
+  def isEven(n:Int):Boolean=n match{
+    case 0 => true
+    case _ => isOdd(n-1)
+  }
+  def isOdd(n:Int):Boolean= !(isEven(n))
+
+  println(isEven(2), isEven(3))
+  println(isOdd(2), isOdd(3))
+}
