7562c450cd5a39d32d355e18bba356d84c5057bd
Roneki Saranga Manamperi <roneki.saranga12@gmail.com>
2021-07-18 04:47:43 +0530

    Problem5

---
+++ Problem5/problem5.scala
@@ -0,0 +1,15 @@
+object problem5 extends App{
+  def isEven(n:Int):Boolean=n match{
+    case 0 => true
+    case _ => isOdd(n-1)
+  }
+  def isOdd(n:Int):Boolean= !(isEven(n))
+
+  def evenSum(n:Int, sum:Int=0):Int={
+    if(n==0)sum
+    else  if (isEven(n)) evenSum(n-1,sum+n)
+    else evenSum(n-1,sum)
+  }
+
+   println(evenSum(10))
+}
